3e42f7ed87b0622dff3e45b73ad1bdf1d69c9e5a
milkashpilka <milenamedvedenko7@gmail.com>
2023-09-11 02:24:45 +0500

    lol

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку:");
+        string input = Console.ReadLine();
+
+        if (IsCharacter(input))
+        {
+            Console.WriteLine("Строка является символом.");
+        }
+        else if (IsInteger(input))
+        {
+            Console.WriteLine("Строка является целым числом.");
+        }
+        else if (IsDouble(input))
+        {
+            Console.WriteLine("Строка является дробным числом.");
+        }
+        else if (IsBoolean(input))
+        {
+            Console.WriteLine("Строка является логическим значением (true/false).");
+        }
+        else
+        {
+            Console.WriteLine("Строка не соответствует ни одному из проверенных типов.");
+        }
+    }
+
+    static bool IsCharacter(string input)
+    {
+        char result;
+        return char.TryParse(input, out result);
+    }
+
+    static bool IsInteger(string input)
+    {
+        int result;
+        return int.TryParse(input, out result);
+    }
+
+    static bool IsDouble(string input)
+    {
+        double result;
+        return double.TryParse(input, out result);
+    }
+
+    static bool IsBoolean(string input)
+    {
+        bool result;
+        return bool.TryParse(input, out result);
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int newInt = 10;
+        float newFloat = 8.4f; // Обратите внимание на суффикс 'f' для указания на тип float
+        string newStr = "True";
+
+        // 2. Увеличьте значение, хранимое в переменной newInt, в 3.5 раза.
+        float bigInt = newInt * 3.5f;
+
+        // 3. Измените значение, хранимое в переменной newFloat, уменьшив его на единицу, результат свяжите с той же переменной.
+        newFloat -= 1;
+
+        // 4. Разделите newInt на newFloat, а затем bigInt на newFloat. Результаты данных выражений выведите на экран.
+        Console.WriteLine("Результат деления newInt на newFloat: " + (newInt / newFloat));
+        Console.WriteLine("Результат деления bigInt на newFloat: " + (bigInt / newFloat));
+
+        // 5. Измените значение переменной newStr на "NoNoYesYesYes". При формировании нового значения используйте операции конкатенации (+).
+        newStr = "NoNo" + newStr + "YesYesYes";
+
+        // 6. Выведите значения всех переменных.
+        Console.WriteLine("newInt: " + newInt);
+        Console.WriteLine("newFloat: " + newFloat);
+        Console.WriteLine("bigInt: " + bigInt);
+        Console.WriteLine("newStr: " + newStr);
+    }
+}
