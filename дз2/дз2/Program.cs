26d3fbdcbc4587bd500db3582c86ef6d98f583d9
milkashpilka <milenamedvedenko7@gmail.com>
2023-09-18 23:06:55 +0500

    dz

---
+++ дз2/дз2/Program.cs
@@ -0,0 +1,19 @@
+﻿using may;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        int temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+
+        Console.WriteLine($"Теперь первое число равно {firstNumber}, а второе равно {secondNumber}");
+    }
+}
