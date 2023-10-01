8be41e241d06a53b78b50df6b86347ed37f71271
milkashpilka <milenamedvedenko7@gmail.com>
2023-10-01 23:31:52 +0500

    я люблю сишарп

---
+++ dz01.6/dz01.6/Program.cs
@@ -0,0 +1,13 @@
+﻿string input = "Это:пример!строки";
+int length = input.Length;
+int halfLength = length / 2;
+
+string firstHalf = input.Substring(0, halfLength);
+string secondHalf = input.Substring(halfLength);
+
+firstHalf = firstHalf.Replace(":", "*");
+secondHalf = secondHalf.Replace("!", ".");
+
+string result = firstHalf + secondHalf;
+
+Console.WriteLine(result);
