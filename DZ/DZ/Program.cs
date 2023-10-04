1f62c7ea76d29df1eb2e9806befc2f85b752be8d
milkashpilka <milenamedvedenko7@gmail.com>
2023-10-04 20:42:49 +0500

    dz

---
+++ DZ/DZ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string input1 = ":)";
+        string input2 = ";---------[[[[[[[[";
+        string input3 = ";--";
+        string input4 = ":-)]";
+        string input5 = "-)";
+        string input6 = "::-(";
+        string input7 = ":-()";
+
+        Console.WriteLine(IsSmiley(input1)); // true
+        Console.WriteLine(IsSmiley(input2)); // true
+        Console.WriteLine(IsSmiley(input3)); // false
+        Console.WriteLine(IsSmiley(input4)); // false
+        Console.WriteLine(IsSmiley(input5)); // false
+        Console.WriteLine(IsSmiley(input6)); // false
+        Console.WriteLine(IsSmiley(input7)); // false
+    }
+
+    static bool IsSmiley(string input)
+    {
+        // Паттерн для смайликов
+        string pattern = "^[;:](-*)[\\[\\]()]+$";
+
+        // Проверка с помощью регулярного выражения
+        return Regex.IsMatch(input, pattern);
+    }
+}
